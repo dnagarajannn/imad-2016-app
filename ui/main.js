3e466db1beb4d7ebf8c16e5e4aaf49f52a0208b7
dnagarajannn <dnagarajannn@gmail.com>
2016-10-01 22:13:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,17 @@
-console.log('Loaded!');
+// counter code
+
+var button = document.getElementById('counter');
+var counter=0;
+Button.onclick = function {
+    // make a request
+    
+    // capture the response
+    
+    // render the variable
+    counter = counter+1;
+    
+  var span = document.getElementById('count')  ;
+  span.innterHTML = counter.toString();
+  
+};
 
