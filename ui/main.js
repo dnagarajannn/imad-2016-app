3be7cd42bc18499669cd20a3addc00b1944c669d
dnagarajannn <dnagarajannn@gmail.com>
2016-10-02 11:46:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,25 @@
 // counter code
 var button = document.getElementById('counter');
+var counter = 0;
 
 button.onclick = function() {
     // create  a request
-var request = new XMLHttpRequest();
+//  var request = new XMLHttpRequest();
     // capture the response
-    request.onreadystatechange = function() {
-        if ( request.readyState === XMLHttpRequest.DONE) {
+//    request.onreadystatechange = function() {
+ //       if ( request.readyState === XMLHttpRequest.DONE) {
             // take some action
-            if ( request.status === 200 ) {
-                var counter = request.responseText;
+  //          if ( request.status === 200 ) {
+ //                var counter = request.responseText;
+ counter = counter + 1;
                 var span = document.getElementById('count');
                  span.innterHTML = counter.toString();  
-                 }
-        }
+ //                }
+ //       }
         // not done yet
     };
     // render the variable
-    request.open('GET','http://dnagarajannn.imad.hasura-app.io/counter',true);
-    request.send(null);
+ //   request.open('GET','http://dnagarajannn.imad.hasura-app.io/counter',true);
+ //   request.send(null);
 };
 
