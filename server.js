276ee0e5a1ee5257689b8c6c3573dd53db914cd1
dnagarajannn <dnagarajannn@gmail.com>
2016-10-24 08:34:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.get('/counter',function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 
 
 app.get('/test-db',function(req,res) {
